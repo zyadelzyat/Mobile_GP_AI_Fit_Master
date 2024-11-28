a35679ae9fc907ac418c8f059a8c10b58c750a4f
zyad elzyat <zyadwaleedkamel.com>
2024-11-28 17:45:44 +0200

    homepage01

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '01_signin_screen.dart';  // Import the SignIn screen
+import '00_home_page.dart';  // Import the SignIn screen
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Onboarding Screen',
-      home: SignInScreen(),  // Set SignInScreen as the initial screen
+      home: HomePage(),  // Set SignInScreen as the initial screen
       debugShowCheckedModeBanner: false,
     );
   }
